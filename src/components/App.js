a25980491f257c7e6af91403e408d9e93b603227
Amritraj96 <amritraj.up.yadav@gmail.com>
2026-01-05 23:04:53 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -13,7 +13,7 @@ function App() {
             <Link to="/">Home</Link>
           </li>
           <li>
-            <Link to="/about">About</Link>
+            <Link to="/about">Abou</Link>
           </li>
         </ul>
       </nav>
